e5b02357176a3c5074df7068044711b3aff0e347
Gabriela <gabyplasencia1995@gmail.com>
2023-11-02 19:11:14 +0000

    base para la actividad 1 tema 3

--- TEMA 3/actividad-practica/act1.js
+++ TEMA 3/actividad-practica/act1.js
@@ -13,3 +13,36 @@ El formulario debe permitir completar nombre, apellidos, dirección (incluyendo 
 código postal, localidad y municipio) así como teléfono y correo electrónico.
 
 Una vez validador el formulario los resultados se mostrarán en la ventana principal.*/
+
+var formTab;
+
+function openForm() {
+
+let consent = window.confirm("El formulario se abrira en una ventana nueva. ¿Desea continuar?");
+
+    if(consent == true) {
+
+        let windowWidth = 600;
+        let windowHeight = 700;
+        let left = (window.screen.width - windowWidth) / 2;
+        let top = (window.screen.height - windowHeight) / 2;
+    
+        let features = "width=" + windowWidth + ",height=" + windowHeight + ",left=" + left + ",top=" + top;
+        let url = "http://127.0.0.1:5500/TEMA%203/actividad-practica/form.html";
+    
+        formTab = window.open(url, "VentanaEmergente", features);
+
+    } else {
+        alert("No se pudo completar el registro");
+    }
+
+}
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    //setValues();
+    formTab.close();
+});
