2165511cd9d528e60c351c2ce1b31b58b4462547
Gabriela <gabyplasencia1995@gmail.com>
2023-12-07 13:56:23 +0000

    tarea 6

--- TEMA 6/main.js
+++ TEMA 6/main.js
@@ -1,13 +1,13 @@
 const colores = document.querySelectorAll('.colores');
 const pincel = document.getElementById('pincel');
 const tablaDibujo = document.getElementById('tabla-dibujo');
-const colorElegido = document.querySelector('.seleccionado');
 
 const pizarra = {
 
     init: () => {
         pizarra.crearTabla();
         pizarra.seleccionarColor();
+        pizarra.addEvents();
     },
 
     crearTabla: () => {
@@ -29,12 +29,35 @@ const pizarra = {
                     color.classList.remove('seleccionado');
                 })
                 color.classList.add('seleccionado');
+                let colorElegido = document.querySelector('.seleccionado');
+                document.body.style.cursor = window.getComputedStyle(colorElegido).getPropertyValue('cursor');
             })
         })
     },
 
-    pintar: () => {
-        
+    addEvents: () => {
+        tablaDibujo.addEventListener('click', pizarra.estadoPincel);
+        tablaDibujo.addEventListener('mouseover', pizarra.pintar);
+    },
+
+    estadoPincel: () => {
+        if(tablaDibujo.classList.contains('activo')){
+            tablaDibujo.classList.remove('activo');
+            pincel.style.color = 'rgb(199, 9, 9)';
+            pincel.innerHTML = "El pincel esta desactivado.<br>Haz click de nuevo para activarlo";
+        }else {
+            tablaDibujo.classList.add('activo');
+            pincel.style.color = 'rgb(5, 95, 5)';
+            pincel.innerHTML = "El pincel esta activo.<br>Puedes pintar!";
+        }
+    },
+
+    pintar: (e) => {
+        if(!e){return;}
+        if(tablaDibujo.classList.contains('activo') && e.target.tagName == 'TD'){
+            let colorElegido = document.querySelector('.seleccionado');
+            e.target.style.backgroundColor = window.getComputedStyle(colorElegido).getPropertyValue('background-color');
+        }
     }
 }
 
